3c6f8b8025b8015f6c4691deead23f664c2d2877
Christian <cbagdon119@gmail.com>
2025-04-03 17:12:26 -0500

    Disable resolve_ip workaround

--- robot_example/setup_proxy.py
+++ robot_example/setup_proxy.py
@@ -1,5 +1,6 @@
 import os
 import urllib3
+from selenium.webdriver.remote.remote_connection import RemoteConnection
 
 def get_proxy_url():
     proxy_variables = ["http_proxy", "HTTP_PROXY", "https_proxy", "HTTPS_PROXY"]
@@ -9,4 +10,6 @@ def get_proxy_url():
 proxy_url = get_proxy_url() 
 
 if proxy_url:
+    old_remote_init = RemoteConnection.__init__
+    RemoteConnection.__init__ = lambda self, *args, **kwargs: old_remote_init(self, *args, **{**kwargs, 'resolve_ip': False})
     urllib3.PoolManager = lambda *args, **kwargs: urllib3.ProxyManager(proxy_url, *args, **kwargs)
